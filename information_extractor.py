4c172386547f61e119bb8c0e56fafafce9b744ae
alisoltanirad <soltaniradali@gmail.com>
2020-11-14 14:02:50 +0330

    add chunker

--- information_extractor.py
+++ information_extractor.py
@@ -7,11 +7,18 @@ def main():
     download_resources()
 
 
+def chunk(sentence):
+    chunk_grammar = 'NP: {<DT>?<JJ>*<NN>}'
+    chunk_parser = nltk.RegexpParser(chunk_grammar)
+    parsed_sentence = chunk_parser.parse(sentence)
+    return parsed_sentence
+
+
 def preprocess_text(document):
     sentences = nltk.sent_tokenize(document)
     sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
     sentences = [nltk.pos_tag(sentence) for sentence in sentences]
-    print(sentences)
+    return sentences
 
 
 def download_resources():
