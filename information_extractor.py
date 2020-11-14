45f41aea8b4fd8e1aa6591e6a77d19b1fad2339c
alisoltanirad <soltaniradali@gmail.com>
2020-11-14 13:38:58 +0330

    add preprocess_text function

--- information_extractor.py
+++ information_extractor.py
@@ -1,9 +1,22 @@
 # https://github.com/alisoltanirad/Information-Extractor.git
 # Dependencies: nltk
+import ssl
 import nltk
 
 def main():
-    pass
+    download_resources()
+
+
+def preprocess_text(document):
+    sentences = nltk.sent_tokenize(document)
+    sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+    sentences = [nltk.pos_tag(sentence) for sentence in sentences]
+    print(sentences)
+
+
+def download_resources():
+    ssl._create_default_https_context = ssl._create_unverified_context
+    nltk.download('averaged_perceptron_tagger')
 
 
 if __name__ == '__main__':
